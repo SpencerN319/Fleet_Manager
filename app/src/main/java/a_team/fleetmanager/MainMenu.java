93e80f42df66d61c4ab5d01ff69f7ca8d704b525
Spencer Nicol <stnicol@iastate.edu>
2018-06-20 16:58:56 -0500

    Vehicle Selection finished

--- app/src/main/java/a_team/fleetmanager/MainMenu.java
+++ app/src/main/java/a_team/fleetmanager/MainMenu.java
@@ -1,13 +1,88 @@
 package a_team.fleetmanager;
 
+import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
+
+    private Button btn_VehicleData, btn_GasChart, btn_ServiceInfo, btn_AllVehicleInfo, btn_Select;
+    private int car_num;
+    private final int VEHICLE_REQUEST_ID = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        btn_VehicleData = findViewById(R.id.btn_VehicleInfo);
+        btn_GasChart = findViewById(R.id.btn_GasData);
+        btn_ServiceInfo = findViewById(R.id.btn_ServiceInformation);
+        btn_AllVehicleInfo = findViewById(R.id.btn_AllVehicleInformation);
+        btn_Select = findViewById(R.id.btn_SelectVehicle);
+        car_num = 0;
+
+        btn_Select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                select_vehicle();
+            }
+        });
+
+        btn_VehicleData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
+
+        btn_GasChart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
+
+        btn_ServiceInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
+
+        btn_AllVehicleInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
+
     }
+
+    private void select_vehicle(){
+        Intent intent = new Intent(this, Select_Vehicle.class);
+        startActivityForResult(intent, VEHICLE_REQUEST_ID);
+    }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent returnedData){
+        if(resultCode == 0){
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, returnedData);
+        switch (requestCode){
+
+            case VEHICLE_REQUEST_ID:
+                int car_num = returnedData.getIntExtra("vehicle", 0);
+                break;
+        }
+    }
+
+
+
 }
---
+++ app/src/main/java/a_team/fleetmanager/Select_Vehicle.java
@@ -0,0 +1,65 @@
+package a_team.fleetmanager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Select_Vehicle extends Activity {
+    private Button b1, b2, b3, b4, b5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Select_Vehicle);
+
+        b1 = findViewById(R.id.btn_One);
+        b2 = findViewById(R.id.btn_Two);
+        b3 = findViewById(R.id.btn_Three);
+        b4 = findViewById(R.id.btn_Four);
+        b5 = findViewById(R.id.btn_Five);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                return_selection(1);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                return_selection(2);
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                return_selection(3);
+            }
+        });
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                return_selection(4);
+            }
+        });
+
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                return_selection(5);
+            }
+        });
+    }
+
+    private void return_selection(int car_num){
+        Intent intent = new Intent(this, MainMenu.class);
+        intent.putExtra("vehicle", car_num);
+        setResult(2, intent);
+        finish();
+    }
+}
