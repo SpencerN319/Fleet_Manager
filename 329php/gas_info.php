58fdabbf09f68d961479331f6deb30a83d79c7c0
tgm <tgm@iastate.edu>
2018-06-26 17:41:31 -0500

    Update gas_info.php

--- 329php/gas_info.php
+++ 329php/gas_info.php
@@ -1,12 +1,12 @@
 <?php
 include connect.php
 $car_num = $_Post["car_num"];
-$sql = "SELECT day, gallons_used FROM gas_info WHERE".$car_num. " = car_num";
+$sql = "SELECT Day, Gallons_used FROM gas_info WHERE".$car_num. " = car_num";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 		while($row = $result->fetch_assoc()) {
-			$car["day"] = $row["day"];
-			$car["gallons_used"] = $row["gallons_used"];
+			$car["Day"] = $row["Day"];
+			$car["Gallons_used"] = $row["Gallons_used"];
 		}
 }
 echo json_encode($car);
