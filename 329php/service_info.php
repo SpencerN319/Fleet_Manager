1e9f1ef5aa3d4c02e5a9fe1d5c622157009d416a
tgm <tgm@iastate.edu>
2018-06-26 17:38:14 -0500

    fixed date vs data thing

--- 329php/service_info.php
+++ 329php/service_info.php
@@ -1,13 +1,13 @@
 <?php
 include connect.php
 $car_num = $_Post["car_num"];
-$sql = "SELECT Service, Desc, Data FROM service_info WHERE".$car_num. "= car_num";
+$sql = "SELECT Service, Desc, Date FROM service_info WHERE".$car_num. "= car_num";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 		while($row = $result->fetch_assoc()) {
-			$car["service"] = $row["service"];
-			$car["desc"] = $row["desc"];
-			$car["data"] = $row["data"];
+			$car["Service"] = $row["Service"];
+			$car["Desc"] = $row["Desc"];
+			$car["Date"] = $row["Date"];
 		}
 }
 echo json_encode($car);
