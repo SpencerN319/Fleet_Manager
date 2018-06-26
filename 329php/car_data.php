935e479eb93651304b4769b88e2fd655bc2b0d2f
tgm <tgm@iastate.edu>
2018-06-26 17:40:07 -0500

    Update car_data.php

--- 329php/car_data.php
+++ 329php/car_data.php
@@ -5,9 +5,9 @@ $sql = "SELECT Make, Model, Year FROM car_data WHERE".$car_num." = car_num";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 		while($row = $result->fetch_assoc()) {
-			$car["make"] = $row["make"];
-			$car["model"] = $row["model"];
-			$car["year"] = $row["year"];
+			$car["Make"] = $row["Make"];
+			$car["Model"] = $row["Model"];
+			$car["Year"] = $row["Year"];
 		}
 }
 echo json_encode($car);
